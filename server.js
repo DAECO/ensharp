02cbb24cd871950aaafcb083d4ab8ddad8cb5035
KIM DAEHO <rlaeogh89@gmail.com>
2016-04-29 22:37:41 +0900

    change color

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
-
+var colors = require('colors');
 http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.end('<h1>Hello World</h1>');
 }).listen('8888', function(){
-	console.log('create server!!');
+	console.log('create server!!'.green);
 });
